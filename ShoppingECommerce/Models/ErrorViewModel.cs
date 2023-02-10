be038394a915fe8c3b7a26a2bc9077bd8fba83ba
DESKTOP-E3LBU6K\FPT Software <nurulwahida.jasman@fsoft.com.vn>
2023-02-10 11:06:48 +0800

    Add project files.

---
+++ ShoppingECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
